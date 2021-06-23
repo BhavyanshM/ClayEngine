60bb4e70a888ae002f0ce7e59ca75cc148d5908a
BhavyanshM <bhavyanshmishra@gmail.com>
2021-06-22 19:46:32 -0500

    Add Uniform Upload methods for different shader data types.

--- src/ClayEngine/Platforms/OpenGL/OpenGLShader.cpp
+++ src/ClayEngine/Platforms/OpenGL/OpenGLShader.cpp
@@ -128,7 +128,43 @@ namespace Clay
       glUseProgram(0);
    }
 
-   void OpenGLShader::UploadUniformMat4(const std::string& name, const glm::mat4& matrix )
+   void OpenGLShader::UploadUniformInt(const std::string& name, int value)
+   {
+      GLint location = glGetUniformLocation(_rendererId, name.c_str());
+      glUniform1i(location, value);
+   }
+
+   void OpenGLShader::UploadUniformFloat(const std::string& name, float value)
+   {
+      GLint location = glGetUniformLocation(_rendererId, name.c_str());
+      glUniform1f(location, value);
+   }
+
+   void OpenGLShader::UploadUniformFloat2(const std::string& name, const glm::vec2& value)
+   {
+      GLint location = glGetUniformLocation(_rendererId, name.c_str());
+      glUniform2f(location, value.x, value.y);
+   }
+
+   void OpenGLShader::UploadUniformFloat3(const std::string& name, const glm::vec3& value)
+   {
+      GLint location = glGetUniformLocation(_rendererId, name.c_str());
+      glUniform3f(location, value.x, value.y, value.z);
+   }
+
+   void OpenGLShader::UploadUniformFloat4(const std::string& name, const glm::vec4& value)
+   {
+      GLint location = glGetUniformLocation(_rendererId, name.c_str());
+      glUniform4f(location, value.x, value.y, value.z, value.w);
+   }
+
+   void OpenGLShader::UploadUniformMat3(const std::string& name, const glm::mat3& matrix)
+   {
+      GLint location = glGetUniformLocation(_rendererId, name.c_str());
+      glUniformMatrix3fv(location, 1, GL_FALSE, glm::value_ptr(matrix));
+   }
+
+   void OpenGLShader::UploadUniformMat4(const std::string& name, const glm::mat4& matrix)
    {
       GLint location = glGetUniformLocation(_rendererId, name.c_str());
       glUniformMatrix4fv(location, 1, GL_FALSE, glm::value_ptr(matrix));
