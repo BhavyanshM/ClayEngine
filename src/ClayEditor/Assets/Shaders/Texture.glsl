167f16d13b8429b69c149dd686a3839b7859e945
BhavyanshM <bhavyanshmishra@gmail.com>
2021-07-13 01:54:04 -0500

    Add temporary support for point clouds 2d.

--- src/ClayEditor/Assets/Shaders/Texture.glsl
+++ src/ClayEditor/Assets/Shaders/Texture.glsl
@@ -36,5 +36,5 @@ in float v_TilingFactor;
 uniform sampler2D u_Textures[32];
 
 void main(){
-  color = texture(u_Textures[int(v_TexIndex)], v_TexCoord * v_TilingFactor) * v_Color;
+    color = texture(u_Textures[int(v_TexIndex)], v_TexCoord * v_TilingFactor) * v_Color;
 }
--- src/ClayEngine/Platforms/OpenGL/OpenGLRendererAPI.cpp
+++ src/ClayEngine/Platforms/OpenGL/OpenGLRendererAPI.cpp
@@ -27,6 +27,7 @@ namespace Clay
       glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
       glEnable(GL_DEPTH_TEST);
+      glEnable(GL_PROGRAM_POINT_SIZE);
    }
 
    void OpenGLRendererAPI::SetClearColor(const glm::vec4& color)
@@ -47,6 +48,7 @@ namespace Clay
    void OpenGLRendererAPI::DrawIndexed(const Ref<VertexArray>& vertexArray, uint32_t indexCount)
    {
       uint32_t count = (indexCount) ? vertexArray->GetIndexBuffer()->GetCount() : indexCount;
+//      glPointSize(4.0); // Also set GL_TRIANGLES below
       glDrawElements(GL_TRIANGLES, count, GL_UNSIGNED_INT, nullptr);
       glBindTexture(GL_TEXTURE_2D, 0);
    }
