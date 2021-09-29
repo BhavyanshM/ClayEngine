7fc3498cb5d1f4d72f8f1f490c2c54d3a3ddfd94
Duncan Calvert <dcalvert@ihmc.us>
2021-09-28 22:22:36 -0500

    Add JavaTools just as temporary thing to try and wrap.

---
+++ src/ClayEngine/Tools/JavaTools.cpp
@@ -0,0 +1,13 @@
+//
+// Created by duncan on 9/28/21.
+//
+
+#include "JavaTools.h"
+
+namespace Clay
+{
+   int GiveOne()
+   {
+      return 1;
+   }
+}
---
+++ src/ClayEngine/Tools/JavaTools.h
@@ -0,0 +1,13 @@
+//
+// Created by duncan on 9/28/21.
+//
+
+#ifndef CLAYENGINE_JAVATOOLS_H
+#define CLAYENGINE_JAVATOOLS_H
+
+namespace Clay
+{
+   int GiveOne();
+}
+
+#endif //CLAYENGINE_JAVATOOLS_H
